da6f37c18fec4357a787fd1607d521eb5f6eac9d
Gavin OMelia <gavin@omelia.org>
2025-02-17 16:11:22 -0500

    Solve recursive fibs

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -11,4 +11,11 @@ class Fibonacci
 
     sequence
   end
+
+  def recursive_fibs(length, a = 0, b = 1, sequence = [])
+    return sequence if sequence.length == length
+
+    sequence <<= a
+    recursive_fibs(length, b, a + b, sequence)
+  end
 end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -3,11 +3,22 @@
 require_relative '../lib/fibonacci'
 
 RSpec.describe Fibonacci do
-  sequence = [0, 1, 1, 2, 3, 5, 8, 13]
-  describe '#fibs' do
+  let(:fibonacci) { Fibonacci.new }
+  let(:sequence) { [0, 1, 1, 2, 3, 5, 8, 13] }
+
+  describe '#iterative_fibs' do
     it 'displays the right sequence' do
-      fibonacci = Fibonacci.new
       expect(fibonacci.iterative_fibs(8)).to eq(sequence)
+    end
+  end
+
+  describe '#recursive_fibs' do
+    it 'displays the right sequence' do
+      expect(fibonacci.recursive_fibs(8)).to eq(sequence)
+    end
+
+    it 'returns the correct number of items in the sequence' do
+      expect(fibonacci.recursive_fibs(4)).to eq(sequence[0..3])
     end
   end
 end
